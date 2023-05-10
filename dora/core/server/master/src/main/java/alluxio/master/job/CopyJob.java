31598651b6950f5263d3978ac15030f33fb59374
Lucy Ge <lucy.ge@alluxio.com>
2023-05-10 03:19:57 -0700

    1. fix doraloadjob filelisting, currently no partial list capability 2. fix job progress stats 3. add scheduler stats retrieval from webserver

--- dora/core/server/master/src/main/java/alluxio/master/job/CopyJob.java
+++ dora/core/server/master/src/main/java/alluxio/master/job/CopyJob.java
@@ -304,7 +304,9 @@ public class CopyJob extends AbstractJob<CopyJob.CopyTask> {
    * @param task
    */
   public void onTaskSubmitFailure(Task<?> task) {
-    Preconditions.checkState(task instanceof CopyTask);
+    if (!(task instanceof CopyTask)) {
+      throw new IllegalArgumentException("Task is not a CopyTask: " + task);
+    }
     ((CopyTask)task).mRoutes.forEach(this::addToRetry);
   }
 
@@ -395,6 +397,7 @@ public class CopyJob extends AbstractJob<CopyJob.CopyTask> {
   @Override
   public String toString() {
     return MoreObjects.toStringHelper(this)
+        .add("JobId", mJobId)
         .add("Src", mSrc)
         .add("Dst", mDst)
         .add("User", mUser)
